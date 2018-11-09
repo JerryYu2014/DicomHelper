8340d2e0e836a440f096da602b0a7abdeb99cd95
advoster <648542313@qq.com>
2018-11-09 11:28:26 +0800

    Signed-off-by: advoster <648542313@qq.com>

--- lib/writeDicom.py
+++ lib/writeDicom.py
@@ -16,69 +16,65 @@ from PIL import Image
 
 def writeDicom(dataset, outfilename):
 
-    datasetObj = json.loads(dataset)
+    datasetObj = json.loads(dataset.encode('GB18030').decode('iso8859'))
 
+    # 读取模板Dicom文件
     filename = get_testdata_files('color-px.dcm')[0]
-
     ds = pydicom.dcmread(filename)
+
+    # 当前时间
+    dt = datetime.datetime.now()
 
     # print("Setting file meta information...")
     # Populate required values for file meta information
-    # file_meta = Dataset()
-    # file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
-    # file_meta.MediaStorageSOPInstanceUID = "1.2.3"
-    # file_meta.ImplementationClassUID = "1.2.3.4"
-    # file_meta.TransferSyntaxUID = "1.2.840.10008.1.2.5"
-
     # file_meta = ds.file_meta
     # file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
     # file_meta.MediaStorageSOPInstanceUID = "1.2.3"
     # file_meta.ImplementationClassUID = "1.2.3.4"
     # file_meta.TransferSyntaxUID = "1.2.840.10008.1.2.5"
 
-    # Pixel Data
-    # img = Image.open('123.bmp')
-    img = Image.open(datasetObj['ImagePixel']['PixelData'])
-    
-    nparr = np.asarray(img)
-
-    # ds[0x7FE0, 0x0010] = DataElement(0x7FE00010, 'OW', img.tobytes())
-    ds.PixelData = nparr.tobytes()
-
-    # print("Setting dataset values...")
+    print("Setting dataset values...")
     # Create the FileDataset instance (initially no data elements, but file_meta supplied)
     # ds = FileDataset(filename, {}, file_meta=file_meta, preamble=b"\0" * 128)
+    
+    img = Image.open(datasetObj['ImagePixel']['PixelData'])
+    nparr = np.asarray(img)
 
-    # ds.PatientID = "123456"
-    # ds.PatientName = "Test^Firstname"
-
+    # Pixel Data
+    # ds[0x7FE0, 0x0010] = DataElement(0x7FE00010, 'OW', img.tobytes())
+    ds.PixelData = nparr.tobytes()
     # Patient's Name
-    # ds[0x0010, 0x0010] = DataElement(0x00100010, 'PN', PersonNameUnicode(b"Wang^XiaoDong=\xcd\xf5\x5e\xd0\xa1\xb6\xab=", [default_encoding, 'GB18030']))
-    ds[0x0010, 0x0010] = DataElement(0x00100010, 'PN', '张无忌'.encode('GB18030'))
+    ds[0x0010, 0x0010] = DataElement(0x00100010, 'PN', datasetObj['Patient']['PatientName'])
     # Patient ID
-    ds[0x0010, 0x0020] = DataElement(0x00100020, 'LO', '123456')
+    ds[0x0010, 0x0020] = DataElement(0x00100020, 'LO', datasetObj['Patient']['PatientID'])
     # Patient's Birth Date
-    ds[0x0010, 0x0030] = DataElement(0x00100030, 'DA', '20010101')
+    ds[0x0010, 0x0030] = DataElement(0x00100030, 'DA', datasetObj['Patient']['PatientBirthDate'])
     # Patient's Sex
-    ds[0x0010, 0x0040] = DataElement(0x00100040, 'CS', 'male')
+    ds[0x0010, 0x0040] = DataElement(0x00100040, 'CS', datasetObj['Patient']['PatientSex'])
     # Study Date
-    ds[0x0008, 0x0020] = DataElement(0x00080020, 'DA', '20181108')
+    ds[0x0008, 0x0020] = DataElement(0x00080020, 'DA', datasetObj['GeneralStudy']['StudyDate'])
     # Study Time
-    ds[0x0008, 0x0030] = DataElement(0x00080030, 'TM', '201309')
+    ds[0x0008, 0x0030] = DataElement(0x00080030, 'TM', datasetObj['GeneralStudy']['StudyTime'])
     # Accession Number
-    ds[0x0008, 0x0050] = DataElement(0x00080050, 'SH', '6257')
+    ds[0x0008, 0x0050] = DataElement(0x00080050, 'SH', datasetObj['GeneralStudy']['AccessionNumber'])
     # Study Instance UID
     # ds[0x0020, 0x000D] = DataElement(0x0020000D, 'UI', '1.2.840.1.2.8.236.511020181107')
     # Study ID
-    ds[0x0020, 0x0010] = DataElement(0x00200010, 'SH', '6257')
+    ds[0x0020, 0x0010] = DataElement(0x00200010, 'SH', datasetObj['GeneralStudy']['StudyID'])
     # Patient's Age
-    ds[0x0010, 0x1010] = DataElement(0x00101010, 'AS', '34')
+    ds[0x0010, 0x1010] = DataElement(0x00101010, 'AS', datasetObj['PatientStudy']['PatientAge'])
+    # Patient's Size
+    ds[0x0010, 0x1020] = DataElement(0x00101020, 'DS', datasetObj['PatientStudy']['PatientSize'])
+    # Patient's Weight
+    ds[0x0010, 0x1030] = DataElement(0x00101030, 'DS', datasetObj['PatientStudy']['PatientWeight'])
     # Series Date
-    ds[0x0008, 0x0021] = DataElement(0x00080021, 'DA', '20181107')
+    ds[0x0008, 0x0021] = DataElement(0x00080021, 'DA', dt.strftime('%Y%m%d'))
     # Series Time
-    ds[0x0008, 0x0031] = DataElement(0x00080031, 'TM', '120709')
+    ds[0x0008, 0x0031] = DataElement(0x00080031, 'TM', dt.strftime('%H%M%S.%f'))
     # Modality
-    ds[0x0008, 0x0060] = DataElement(0x00080060, 'CS', 'US')
+    ds[0x0008, 0x0060] = DataElement(0x00080060, 'CS', datasetObj['GeneralSeries']['Modality'])
+    # Series Description
+    ds[0x0008, 0x103E] = DataElement(0x0008103E, 'LO', '')
     # Performing Physician's Name
     ds[0x0008, 0x1050] = DataElement(0x00081050, 'PN', '')
     # Body Part Examined
@@ -86,44 +82,29 @@ def writeDicom(dataset, outfilename):
     # Series Instance UID
     # ds[0x0020, 0x000E] = DataElement(0x0020000E, 'UI', '1.2.840.1.2.8.236.51102018110715')
     # Series Number
-    ds[0x0020, 0x0011] = DataElement(0x00200011, 'IS', '1')
+    ds[0x0020, 0x0011] = DataElement(0x00200011, 'IS', datasetObj['GeneralSeries']['SeriesNumber'])
     # Manufacturer
-    ds[0x0008, 0x0070] = DataElement(0x00080070, 'LO', 'advoster')
+    ds[0x0008, 0x0070] = DataElement(0x00080070, 'LO', datasetObj['GeneralEquipment']['Manufacturer'])
     # Institution Name
-    ds[0x0008, 0x0080] = DataElement(0x00080080, 'LO', '同仁堂'.encode('GB18030'))
+    ds[0x0008, 0x0080] = DataElement(0x00080080, 'LO', datasetObj['GeneralEquipment']['InstitutionName'])
     # Image Type
-    ds[0x0008, 0x0008] = DataElement(0x00080008, 'CS', ['DERIVED', 'SECONDARY'])   # SECONDARY
+    # ds[0x0008, 0x0008] = DataElement(0x00080008, 'CS', ['DERIVED', 'SECONDARY'])   # SECONDARY
     # Instance Number
-    ds[0x0020, 0x0013] = DataElement(0x00200013, 'IS', '1')
-    # # Pixel Data
-    # img = Image.open('123.bmp')
-    # nparr = np.asarray(img)
-
-    # ds[0x7FE0, 0x0010] = DataElement(0x7FE00010, 'OW', nparr.tobytes())
-    # # ds.PixelData = nparr.tobytes()
-
+    ds[0x0020, 0x0013] = DataElement(0x00200013, 'IS', datasetObj['GeneralImage']['InstanceNumber'])
     # Window Center
-    # ds[0x0028, 0x1050] = DataElement(0x00281050, 'DS', "127.0")
+    ds[0x0028, 0x1050] = DataElement(0x00281050, 'DS', datasetObj['VOILUT']['WindowCenter'])
     # Window Width
-    # ds[0x0028, 0x1051] = DataElement(0x00281051, 'DS', "255.0")
+    ds[0x0028, 0x1051] = DataElement(0x00281051, 'DS', datasetObj['VOILUT']['WindowWidth'])
     # # SOP Class UID
     # ds[0x0008, 0x0016] = DataElement(0x00080016, 'UI', '1.2.840.10008.5.1.4.1.1.7')
     # # SOP Instance UID
     # ds[0x0008, 0x0018] = DataElement(0x00080018, 'UI', '1.2.840.1.2.8.236.511020181107154038')
-    # # Instance Number
-    # ds[0x0020, 0x0013] = DataElement(0x00200013, 'IS', 1)
-
     # Bits Allocated
-    # ds[0x0028, 0x0100] = DataElement(0x00280100, 'US', 16)
+    ds[0x0028, 0x0100] = DataElement(0x00280100, 'US', 8)
     # Rows
     ds[0x0028, 0x0010] = DataElement(0x00280010, 'US', 480)
     # Columns
     ds[0x0028, 0x0011] = DataElement(0x00280011, 'US', 640)
-
-    # # Series Instance UID
-    # ds[0x0020, 0x000E] = DataElement(0x0020000E, 'UI', '1.2.840.10008.5.1.4.1.2.5')
-    # # Study Instance UID
-    # ds[0x0020, 0x000D] = DataElement(0x0020000D, 'UI', '1.2.840.10008.5.1.4.1.2.1')
     # Photometric Interpretation
     ds[0x0028, 0x0004] = DataElement(0x00280004, 'UI', 'RGB')
     # Samples Per Pixel
@@ -131,18 +112,19 @@ def writeDicom(dataset, outfilename):
     # Pixel Aspect Ratio
     ds[0x0028, 0x0034] = DataElement(0x00280034, 'IS', ['1', '1'])
     # Bits Stored
-    # ds[0x0028, 0x0101] = DataElement(0x00280101, 'US', 1)
+    ds[0x0028, 0x0101] = DataElement(0x00280101, 'US', 8)
     # High Bit
-    # ds[0x0028, 0x0102] = DataElement(0x00280102, 'US', 19)
+    ds[0x0028, 0x0102] = DataElement(0x00280102, 'US', 7)
 
-    # dt = datetime.datetime.now()
-    # ds.ContentDate = dt.strftime('%Y%m%d')
-    # ds.ContentTime = dt.strftime('%H%M%S.%f')  # long format with micro seconds
+    ds.ContentDate = dt.strftime('%Y%m%d')
+    ds.ContentTime = dt.strftime('%H%M%S.%f')  # long format with micro seconds
 
     ds.save_as(outfilename)
 
     # pydicom.write_file(filename, ds)
 
-    print('Load file {} ...'.format(outfilename))
-    ds = pydicom.dcmread(outfilename)
-    print(ds)
+    # print('Load file {} ...'.format(outfilename))
+    # ds = pydicom.dcmread(outfilename)
+    # print(str(ds).encode('iso8859').decode('iso8859'))
+
+    print("Dicom文件：{} 生成成功！".format(outfilename))
--- scripts/writeDicom.py
+++ scripts/writeDicom.py
@@ -8,7 +8,6 @@ __version__ = '1.0.1'
 
 class WriteDicom(object):
     arguments = None
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')  # 改变默认的标准输出
 
     def __init__(self, subparser, command, description='default'):
         self.parse_arguments(description, subparser, command)
@@ -20,8 +19,8 @@ class WriteDicom(object):
             description=description
         )
 
-        parser.add_argument("-d", "--dataset", dest="dataset", default='', help="dataset for write dicom file")
-        parser.add_argument("-f", "--filename", dest="filename", default='', help="filename for write dicom file")
+        parser.add_argument("-d", "--dataset", dest="dataset", default='', help="the dataset for write dicom file")
+        parser.add_argument("-f", "--filename", dest="filename", default='', help="the filename of output dicom file")
         parser.add_argument("-v", '--version', action='version', version="WriteDicom version" + __version__)
 
         parser = self.add_optional_arguments(parser)
@@ -37,7 +36,6 @@ class WriteDicom(object):
         try:
             if self.arguments.dataset and self.arguments.filename:
                 writeDicom(self.arguments.dataset, self.arguments.filename)
-            
         except Exception as e:
             print(e)
 
